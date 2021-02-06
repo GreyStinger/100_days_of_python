49a459652019e9c7b8ae2a36a706727511bcbd0c
GreyStinger <greystinger@gmail.com>
2021-02-06 17:33:02 +0200

    Beginning of of final task updates

---
+++ Day 10/main.py
@@ -0,0 +1,30 @@
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 + n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+num1 = int(input("What's the first number?: "))
+num2 = int(input("What's the second number?: "))
+
+for key in operations:
+    print(key)
+
+operation_symbol = str(input("Pick an operation_symbol: "))
