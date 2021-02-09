40cb82b38cce41f7a8786af642de6b28e1ffe289
GreyStinger <greystinger@gmail.com>
2021-02-09 13:53:52 +0200

    Added a calculation history for a continued calculation

--- Day 10/main.py
+++ Day 10/main.py
@@ -38,11 +38,16 @@ def main():
     first_operation = True
 
     previous_answer = int
+    previous_operations = []
 
     while continue_on is True:
         print(art.logo + "\n")
 
-        if continue_on is True and first_operation is not True:
+        if first_operation is False:
+            print("Your previous calculations were:")
+            for string_number in range(len(previous_operations)):
+                print(previous_operations[string_number])
+            print("")
             num1 = previous_answer
             print(f"The first number is {previous_answer}")
         else:
@@ -56,25 +61,34 @@ def main():
         operation_symbol = str(input("Pick an operation_symbol: "))
         print("")
 
+        if operation_symbol not in operations:
+            print("Please enter a valid operational symbol.\nThe program will be restarted.\n")
+            time.sleep(8)
+            continue
+
         num2 = int(input("What's the second number?: "))
         print("")
 
         answer = operations[operation_symbol](num1, num2)
 
-        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        current_operation = f"{num1} {operation_symbol} {num2} = {answer}"
+
+        print(current_operation)
         print("\nHere are your options forward:\n\n1. Continue the calculation using the previous answer.\n2. Start a "
               "new calculation.\n3. Quit the application.\n")
 
         stop_or_continue = str(input("Answer with either '1', '2' or '3', or 'continue', 'new' or 'stop': ")).lower()
 
         if stop_or_continue == "1" or stop_or_continue == "continue":
-            first_operation = False
             grey.clear()
+            first_operation = False
             previous_answer = answer
+            previous_operations.append(current_operation)
         elif stop_or_continue == "2" or stop_or_continue == "new":
             grey.clear()
             previous_answer = int
             first_operation = True
+            previous_operations = []
         elif stop_or_continue == "3" or stop_or_continue == "stop":
             print("Quitting")
             time.sleep(2)
@@ -88,4 +102,5 @@ def main():
             first_operation = True
 
 
-main()
+if __name__ == '__main__':
+    main()
