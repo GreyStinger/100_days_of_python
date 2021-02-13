fe28113f7097f490d16b73a3c253ebbba079faf5
GreyStinger <greystinger@gmail.com>
2021-02-13 11:45:16 +0200

    updates

--- Day 14/main.py
+++ Day 14/main.py
@@ -1,2 +1,7 @@
-# This is a sample Python script.
--------------------
+# TODO: Write a dictionary of instagram accounts and their followers.
+# TODO: Create a function to sort through the dictionary and edit it.
+# TODO: Make sure that you don't pick duplicates from your dictionary.
+# TODO: Create the comparison function.
+# TODO: Use the grey package for clearing the screen after each comparison.
+# TODO: Add a counter that counts how much score the user has.
+# TODO: Add a comparison function using user inputs as 'A' and 'B'.
