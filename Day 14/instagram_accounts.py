468eb85a11b6d14e6939a366c8ace8702ea3565b
GreyStinger <greystinger@gmail.com>
2021-02-15 13:40:00 +0200

    Completed just adding finishing touches

---
+++ Day 14/instagram_accounts.py
@@ -0,0 +1,144 @@
+# noinspection SpellCheckingInspection
+users = {
+    1: {
+        "name": "hyperx", "readout": "1 700 000 million", "number": 1700000
+    },
+    2: {
+        "name": "nvidiageforce", "readout": "2 200 000 million", "number": 2200000
+    },
+    3: {
+        "name": "nasa", "readout": "62 900 000 million", "number": 62900000
+    },
+    4: {
+        "name": "ea.dice", "readout": "115 000 thousand", "number": 115000
+    },
+    5: {
+        "name": "doom", "readout": "451 000 thousand", "number": 451000
+    },
+    6: {
+        "name": "zenimax.online", "readout": "2 934 thousand", "number": 2934
+    },
+    7: {
+        "name": "insomniacgames", "readout": "485 000 thousand", "number": 485000
+    },
+    8: {
+        "name": "infinityward", "readout": "924 000 thousand", "number": 924000
+    },
+    9: {
+        "name": "aorus_official", "readout": "1 400 000 million", "number": 1400000
+    },
+    10: {
+        "name": "rainbow6__", "readout": "739 000 thousand", "number": 739000
+    },
+    11: {
+        "name": "squareenix", "readout": "1 000 000 million", "number": 1000000
+    },
+    12: {
+        "name": "halo", "readout": "1 200 000 million", "number": 1200000
+    },
+    13: {
+        "name": "inteldeveloperzpne", "readout": "444 000 thousand", "number": 444000
+    },
+    14: {
+        "name": "bandinamcous", "readout": "1 200 000 million", "number": 1200000
+    },
+    15: {
+        "name": "rainbow6game_us", "readout": "2 100 000 million", "number": 2100000
+    },
+    16: {
+        "name": "battlefield", "readout": "1 900 000 million", "number": 1900000
+    },
+    17: {
+        "name": "pubg", "readout": "5 300 000 million", "number": 5300000
+    },
+    18: {
+        "name": "callofduty", "readout": "11 100 000 million", "number": 11100000
+    },
+    19: {
+        "name": "twitch", "readout": "5 500 000 million", "number": 5500000
+    },
+    20: {
+        "name": "ubisoftnl", "readout": "637 000 thousand", "number": 637000
+    },
+    21: {
+        "name": "ubisoft", "readout": "6 600 000 million", "number": 6600000
+    },
+    22: {
+        "name": "cloud9gg", "readout": "702 000 thousand", "number": 702000
+    },
+    23: {
+        "name": "razer", "readout": "6 700 000 million", "number": 6700000
+    },
+    24: {
+        "name": "teamliquid", "readout": "1 000 000 million", "number": 1000000
+    },
+    25: {
+        "name": "fnatic", "readout": "1 200 000 million", "number": 1200000
+    },
+    26: {
+        "name": "logitechg", "readout": "3 200 000 million", "number": 3200000
+    },
+    27: {
+        "name": "g2esports", "readout": "1 200 000 million", "number": 1200000
+    },
+    28: {
+        "name": "eleaguetv", "readout": "180 000 thousand", "number": 180000
+    },
+    29: {
+        "name": "eslcs", "readout": "374 000 thousand", "number": 374000
+    },
+    30: {
+        "name": "easportsfifa", "readout": "22 300 000 million", "number": 22300000
+    },
+    31: {
+        "name": "nvidia", "readout": "1 900 000 million", "number": 190000
+    },
+    32: {
+        "name": "corsair", "readout": "2 600 000 million", "number": 2600000
+    },
+    33: {
+        "name": "dccomics", "readout": "10 900 000 million", "number": 10900000
+    },
+    34: {
+        "name": "gigabyte_official", "readout": "1 100 000 million", "number": 1100000
+    },
+    35: {
+        "name": "asusrog", "readout": "2 500 000 million", "number": 2500000
+    },
+    36: {
+        "name": "msius", "readout": "1 100 000 million", "number": 1100000
+    },
+    37: {
+        "name": "msigaming", "readout": "1 400 000 million", "number": 1400000
+    },
+    38: {
+        "name": "asus", "readout": "2 500 000 million", "number": 2500000
+    },
+    39: {
+        "name": "playstation", "readout": "28 600 000 million", "number": 28600000
+    },
+    40: {
+        "name": "sony", "readout": "8 700 000 million", "number": 8700000
+    },
+    41: {
+        "name": "blizzard", "readout": "804 000 thousand", "number": 804000
+    },
+    42: {
+        "name": "activision", "readout": "1 200 000 million", "number": 1200000
+    },
+    43: {
+        "name": "ea", "readout": "3 100 000 million", "number": 3100000
+    },
+    44: {
+        "name": "rockstargames", "readout": "19 500 000 million", "number": 19500000
+    },
+    45: {
+        "name": "nintendo", "readout": "6 900 000 million", "number": 6900000
+    },
+    46: {
+        "name": "biowarebase", "readout": "171 000 thousand", "number": 171000
+    },
+    47: {
+        "name": "markiplier", "readout": "8 600 000 million", "number": 8600000
+    }
+}
--- Day 14/main.py
+++ Day 14/main.py
@@ -1,7 +1,89 @@
-# TODO: Write a dictionary of instagram accounts and their followers.
-# TODO: Create a function to sort through the dictionary and edit it.
-# TODO: Make sure that you don't pick duplicates from your dictionary.
-# TODO: Create the comparison function.
-# TODO: Use the grey package for clearing the screen after each comparison.
-# TODO: Add a counter that counts how much score the user has.
-# TODO: Add a comparison function using user inputs as 'A' and 'B'.
+import random
+
+import art
+import game_data
+import grey
+
+
+def dictionary_delete(accounts, to_delete):
+    print(to_delete)
+    accounts.remove(to_delete)
+
+
+def higher(a, b):
+    """Takes two integers as input's named a and b then returns a lowercase string of either 'a' or 'b' depending on
+    which was higher"""
+    number = a * (a > b) + b * (a <= b)
+    if number == a:
+        return 'a'
+    else:
+        return 'b'
+
+
+def select_new_comparison(accounts):
+    return random.choice(accounts)
+
+
+def compare():
+    """Is the main comparison function of this game. Can be run standalone, does not require main() to be run to
+    function correctly."""
+    resume = True
+    while resume is True:
+        accounts = game_data.data.copy()
+        current_compare = select_new_comparison(accounts=accounts)
+        score = 0
+        failed = False
+        while failed is False:
+
+            current_against = select_new_comparison(accounts)
+            higher_comparison = higher(current_compare['follower_count'], current_against['follower_count'])
+
+            print(current_compare, current_against)
+            print(f"{art.logo}\n")
+
+            if score != 0:
+                print(f"You're Right! Your current score is {score}\n")
+            else:
+                print(f"Welcome. Your score will show here on your second round.\n")
+
+            print(f"Compare A: {current_compare['name']}, a {current_compare['description']} from "
+                  f"{current_compare['country']}\n")
+            print(art.vs)
+            print(f"Against B: {current_against['name']}, a {current_against['description']} from "
+                  f"{current_against['country']}")
+
+            choice = input("\nWho has more follower's? Type 'A' or 'B': ").lower()
+
+            if higher_comparison == 'a' and choice == 'b' or higher_comparison == \
+                    'b' and choice == 'a':
+                grey.clear()
+                print(f"{art.logo}\nUnfortunately you chose incorrectly.\nYour final score was {score}.\n")
+                failed = True
+                continue
+
+            print("You guessed correctly, well done.")
+            score += 1
+
+            if choice == 'a':
+                to_delete = current_against
+                dictionary_delete(accounts=accounts, to_delete=to_delete)
+            else:
+                to_delete = current_compare
+                dictionary_delete(accounts=accounts, to_delete=to_delete)
+                current_compare = current_against
+
+        while True:
+            choice = str(input("Would you like to play again?\nAnswer with 'y' or 'n'").lower())
+            if choice == 'n':
+                resume = False
+                break
+            elif choice == 'y':
+                break
+            else:
+                print("Please enter a valid input.")
+
+
+if __name__ == '__main__':
+    compare()
+
+# TODO: Add a system that if there are no more lists to compare then the player has won.
