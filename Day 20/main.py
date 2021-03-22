0cbe906bd4a997db69fec43c0c0906b63888f937
grey <greystinger@gmail.com>
2021-03-22 15:33:14 +0200

    moved screen out of snake.py

--- Day 20/main.py
+++ Day 20/main.py
@@ -1,10 +1,31 @@
+from turtle import Screen
 import snake as s
 
 snake = s.Snake()
 
 
+def window(left, right, new, end):
+    screen = Screen()
+    screen.listen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor('black')
+    screen.title('Snake')
+    screen.mode('logo')
+    screen.tracer(0)
+    screen.listen()
+    screen.onkeypress(key='a', fun=left)
+    screen.onkeypress(key='d', fun=right)
+    screen.onkeypress(key='s', fun=new)
+    screen.onkeypress(key='Escape', fun=end)
+    return screen
+
+
+window = window(left=snake.left, right=snake.right, new=snake.new_snake, end=snake.end)
+
+
 def main():
-    snake.run()
+    while snake.play:
+        snake.move()
 
 
 if __name__ == '__main__':
--- Day 20/snake.py
+++ Day 20/snake.py
@@ -18,27 +18,9 @@ def snake_create(coords, heading=90.0):
     return snake_section
 
 
-def window(left, right, new, end):
-    screen = Screen()
-    screen.listen()
-    screen.setup(width=600, height=600)
-    screen.bgcolor('black')
-    screen.title('Snake')
-    screen.mode('logo')
-    screen.tracer(0)
-    screen.listen()
-    screen.onkeypress(key='a', fun=left)
-    screen.onkeypress(key='d', fun=right)
-    screen.onkeypress(key='s', fun=new)
-    screen.onkeypress(key='Escape', fun=end)
-    return screen
-
-
 class Snake:
     def __init__(self):
-        self.window = window(left=self.left, right=self.right, new=self.new_snake, end=self.end)
-        self.snake = [snake_create((0, 0)), snake_create((-20, 0)), snake_create((-40, 0)),
-                      snake_create((-60, 0))]
+        self.snake = [snake_create((0, 0)), snake_create((-20, 0)), snake_create((-40, 0)), snake_create((-60, 0))]
         self.play = True
 
     def new_snake(self):
@@ -51,17 +33,13 @@ class Snake:
     def left(self):
         self.snake[0].setheading(self.snake[0].heading() - 90)
 
-    def run(self):
+    def move(self):
+        for m in reversed(range(1, len(self.snake))):
+            self.snake[m].goto(self.snake[m - 1].pos())
 
-        while self.play:
-            for m in reversed(range(1, len(self.snake))):
-                self.snake[m].goto(self.snake[m - 1].pos())
+        self.snake[0].fd(average_movement)
 
-            self.snake[0].fd(average_movement)
-
-            time.sleep(0.08)
-
-            self.window.update()
+        time.sleep(0.08)
 
     def end(self):
         self.play = False
